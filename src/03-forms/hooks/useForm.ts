f58f1979d34bdb4363f93fe40a6bf987ba22b813
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2022-08-22 18:45:18 -0300

    custom useForms

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -1,6 +1,45 @@
-export const useForm = () => {
-  
-    return {
+import { ChangeEvent, useState } from "react";
 
+export const useForm = <T>(initialData: T) => {
+  const [formData, setFormData] = useState<T>(initialData);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData((data) => ({
+      ...data,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const resetForm = () => {
+    setFormData(initialData);
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  const isEmpty = (cadena: string): boolean => {
+    return cadena.trim().length <= 0;
+  };
+
+  const isValidLength = (cadena: string, length: number): boolean => {
+    return cadena.trim().length >= length;
+  }; 
+
+  const areEqual = (cadena1: string, cadena2: string): boolean => {
+    return cadena1 === cadena2;
   }
-}
+
+  return {
+    formData,
+    ...formData,
+    onChange,
+    resetForm,
+    isValidEmail,
+    isEmpty,
+    isValidLength,
+    areEqual
+  };
+};
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import '../styles/styles.css';
+import { useForm } from '../hooks/useForm';
 
 interface RegisterData {
     name: string;
@@ -10,25 +11,27 @@ interface RegisterData {
 
 export const RegisterPage = () => {
 
-    const [registerData, setRegisterData] = useState<RegisterData>({
+    const { 
+        formData, 
+        onChange, 
+        resetForm, 
+        isValidEmail, 
+        isEmpty,
+        isValidLength,
+        areEqual,
+        name, 
+        email, 
+        password1, 
+        password2} = useForm<RegisterData>({
         name: '',
         email: '',
         password1: '',
         password2: ''
     });
 
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setRegisterData(data => ({
-            ...data,
-            [event.target.name]: event.target.value
-        }));
-    }
-
-    const { name, email, password1, password2 } = registerData;
-
     const onSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(registerData);
+        console.log(formData);
         
     }
 
@@ -42,28 +45,40 @@ export const RegisterPage = () => {
                     placeholder='Name'
                     name='name'
                     value={name}
-                    onChange={onChange} />
+                    onChange={onChange}
+                    className={`${isEmpty(name) && 'has-error'}`} />
+                {isEmpty(name) && <span>* Este campo es necesario</span>}
 
                 <input type="text"
                     placeholder='Email'
                     name='email'
                     value={email}
-                    onChange={onChange} />
+                    onChange={onChange}
+                    className={`${ !isValidEmail(email) && 'has-error'}`} />
+                { !isValidEmail(email) && <span>* Este email no es valido</span>}
 
                 <input type="password"
                     placeholder='Password'
                     name='password1'
                     value={password1}
-                    onChange={onChange} />
+                    onChange={onChange}
+                    className={`${!isValidLength(password1, 6) && 'has-error'}`} />
+                {!isValidLength(password1, 6) && <span>* La contraseña debe ser de 6 caracteres por lo menos</span>}
 
                 <input type="password"
                     placeholder='Repeat Password'
                     name='password2'
                     value={password2}
-                    onChange={onChange} />
+                    onChange={onChange}
+                    className={`${!areEqual(password1, password2) && 'has-error'}`} />
+                {!areEqual(password1, password2) && <span>Las contreseñas no coinciden</span>}
 
                 <button type='submit'>
                     Create
+                </button>
+
+                <button onClick={resetForm}>
+                    Reset
                 </button>
             </form>
         </div>
